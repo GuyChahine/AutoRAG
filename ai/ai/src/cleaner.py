63961cba2521f2905d15a074bb8253978d0b1913
Guy Chahine <guychahine@gmail.com>
2024-12-02 20:32:38 +0100

    fix: backslash n and space between html tags

--- ai/ai/src/cleaner.py
+++ ai/ai/src/cleaner.py
@@ -5,11 +5,11 @@ import re
 class HtmlSimpleCleaner:
 
     def __init__(self):
-        self.cleaning_regex = re.compile(r"(?:\\n|[\ \\\"\'\r])+")
+        self.cleaning_regex = re.compile(r"(?:\n|[\ \\\"\'\r])+")
 
     def __call__(self, html: str) -> str:
         soup = BeautifulSoup(html, features="html.parser")
-        document = soup.get_text()
+        document = soup.get_text(" ")
         if not document:
             raise ValueError(f"No text retrieved from the html.")
         clean_document = self.cleaning_regex.sub(" ", document).strip()
